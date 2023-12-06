195c7bec487232d2e4280b0749cfaccc14f6b2b7
Imtraped <nicholasplouffe87@gmail.com>
2023-12-06 08:59:40 -0500

    Added StrictMode for development

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import StorePicker from './components/StorePicker';
 
 const container = document.querySelector('#main')
 const root = createRoot(container)
-root.render(<StorePicker />);
+root.render(
+    <StrictMode>
+        <StorePicker />
+    </StrictMode>
+    );
