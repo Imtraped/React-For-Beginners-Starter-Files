5d4fb4c5555c37342153072c1048b0bf7e84f32d
Imtraped <nicholasplouffe87@gmail.com>
2023-12-06 12:53:41 -0500

    Styling

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,6 +1,7 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import StorePicker from './components/StorePicker';
+import './css/style.css';
 
 const container = document.querySelector('#main')
 const root = createRoot(container)
