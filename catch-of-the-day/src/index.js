eda860890e12e902d191264f27fff6db970ccf76
Imtraped <nicholasplouffe87@gmail.com>
2023-12-06 08:05:45 -0500

    Updated to React 18.

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import StorePicker from './components/StorePicker';
 
-render(<StorePicker />, document.querySelector('#main'));
+const container = document.querySelector('#main')
+const root = createRoot(container)
+root.render(<StorePicker />);
